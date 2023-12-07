93c9232e22acca9f1dc833ab61bc7d0e23572a53
TheSmoothWater <152207482+TheSmoothWater@users.noreply.github.com>
2023-12-07 05:29:51 -0500

    Update Autonomous Flight.py

--- CodroneProjectSections/Autonomous Flight.py
+++ CodroneProjectSections/Autonomous Flight.py
@@ -1,61 +1,87 @@
+#Official and Final Code
 from codrone_edu.drone import *
 import time
 
 drone = Drone()
 drone.pair()
-drone.takeoff()
-
-drone.set_throttle(30)
-drone.move(2)
-drone.set_throttle(0)
-drone.hover(1)
+drone.set_initial_pressure()
+pressure = 70
 
 time_start = time.time()
-time_limit = 60 #seconds
-drone_speed = 25
+#Should be battery life time but I forgot (sorry)
+time_limit = 480 #seconds
+
+drone_speed = 30
+sensor_d = 25 #cm
+
+#Lab 8 
+def set_altitude(alt):
+    d_height = drone.height_from_pressure()
+    sign = 1
+    if alt <= d_height:
+        sign = -1
+    drone.set_throttle(sign * 20)
+    drone.move()
+    while sign * alt > sign * d_height:
+        print(d_height, "centimeters")
+        d_height = drone.height_from_pressure()
+        drone.set_throttle(0)
+
 
 def fly_drone():
+    #Take off
+    if drone.detect_wall(5):  # In millimeters with a max of 1500 mm
+        print("Takeoff area is not clear. Shutting Down.")
+        drone.close()
+    else:
+        drone.takeoff()
     time_elapsed = 0
-    while time_elapsed < time_limit:
+
+    set_altitude(pressure) #This sets the drone to the wanted height
+    
+    while time_elapsed < time_limit: #How long the drone will fly for 
         time_elapsed = time.time() - time_start
         drone.set_pitch(drone_speed)
         drone.move()
         time.sleep(0.01)
 
-        if is_object_detected(100):
-            avoid_object(100)
+        if is_object_detected(sensor_d):
+            avoid_object(sensor_d)
 
 
 def is_object_detected(distance):
     if drone.get_front_range("cm") <= distance:
-        return True
+        return True  #Path is not clear
     else:
-        return False
+        return False #Path is clear
 
-
+#In easy terms, the drone will turn left if it can and if not then right and if not then back
 def avoid_object(d_distance):
     drone.set_pitch(0)
-    time.sleep(0.01)
-    distance = d_distance
-
     drone.turn_left()
-    left_result = is_object_detected(distance)
+    left_result = is_object_detected(d_distance)
+    if left_result is False:
+        print(left_result)
+        return True
     drone.turn_right(180)
-    right_result = is_object_detected(distance)
+    right_result = is_object_detected(d_distance)
+    if right_result is False:
+        print(right_result)
+        return True
+    drone.turn_right()
 
-    if (left_result is False and right_result is False) or (left_result is True and right_result is False):
-        return True
-    elif right_result is True and left_result is False:
-        drone.turn_left(180)
-        time.sleep(0.01)
-        return True
-    else:
-        drone.turn_right()
-        time.sleep(0.01)
-        return True
+    # if (left_result is False and right_result is False) or (left_result is True and right_result is False):
+    #     return True
+    # elif right_result is True and left_result is False:
+    #     drone.turn_left(180)
+    #     return True
+    # else:
+    #     drone.turn_right()
+    #     return True
 
 
 fly_drone()
 
 drone.land()
 drone.close()
+
